38a18ee2f0ba3d04bcc7ad36788341fab0c3808e
Jukiro <54697159+JuanQuiro@users.noreply.github.com>
2023-01-07 18:24:00 -0400

    Title

--- src/config.mjs
+++ src/config.mjs
@@ -5,8 +5,8 @@ export const SITE = {
 	basePathname: '/',
 	trailingSlash: false,
 
-	title: 'AstroWind — Your website with Astro + Tailwind CSS',
-	description: '🚀 AstroWind is a free and ready to start template to make your website using Astro and Tailwind CSS.',
+	title: 'Dominio Dragon',
+	description: 'Es una pagina web',
 
 	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
 	googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
