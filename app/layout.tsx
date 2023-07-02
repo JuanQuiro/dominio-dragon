97f4736a4583ab7765d781f932e600477807e1df
juanquiro <juanquirozsanchez@hotmail.com>
2023-07-02 19:24:02 +0000

    fix: :bug: arreglando errores menores

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,26 @@
-import Navbar from '@/components/Navbar'
-import './globals.css'
-import { Josefin_Sans } from 'next/font/google'
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import { Josefin_Sans } from "next/font/google";
 
-const jssans = Josefin_Sans({ subsets: ['latin'] })
+const jssans = Josefin_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Portfolio - Alejandro Dalzotto'
-}
+  title: "Dominio Dragon",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${jssans.className}  bg-gradient-to-tr from-black to-neutral-800 scroll`}>
+      <body
+        className={`${jssans.className}  bg-gradient-to-tr from-black to-neutral-800 scroll`}
+      >
         <Navbar />
         {children}
       </body>
     </html>
-  )
+  );
 }
